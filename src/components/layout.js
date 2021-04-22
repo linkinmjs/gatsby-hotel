bb6d9683c7efbe9c461809edcb1e925aa4516879
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-22 08:18:51 -0300

    Creando la Navegacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = (props) => {
             /> 
             <Helmet>
                 <title>Gatsby hotel</title>
-                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" />
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"  rel="stylesheet" />
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@100;700&display=swap" rel="stylesheet"/>
             </Helmet>
