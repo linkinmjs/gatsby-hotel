3db6a5c341ffa13fafeeb16e14f269984511a311
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-25 08:19:49 -0300

    CSS al Hero

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -4,8 +4,36 @@ import BackgroundImage from 'gatsby-background-image';
 import styled from '@emotion/styled';
 
 const ImageBackground = styled(BackgroundImage)`
-    height: 700px;
-`
+    height: 600px;
+    //height: 700px;
+`;
+
+const TextoImagen = styled.div`
+    background-image: linear-gradient(to top, rgba(34,49,63,.85), rgba(34,49,63,.85));
+    color: #FFF;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    h1{
+        font-size: 4rem;
+        margin: 0%;
+
+        @media (min-width: 992px){
+            font-size: 5.8rem;
+        }
+    }
+    p{
+        font-size: 2rem;
+
+        @media (min-width: 992px){
+            font-size: 2.6rem;
+        }
+    }
+`;
 
 const ImagenHotel = () => {
 
@@ -21,17 +49,20 @@ const ImagenHotel = () => {
         }
     `)
 
-        //console.log(image.sharp.fluid);
+    //console.log(image.sharp.fluid);
 
-    return ( 
+    return (
         <ImageBackground
             tag="section"
             fluid={image.sharp.fluid}
             fadeIn="soft"
         >
-
+            <TextoImagen>
+                <h1>Bienvenido a Hotel Gatsby</h1>
+                <p>El mejor hotel para tus vacaciones</p>
+            </TextoImagen>
         </ImageBackground>
-     );
+    );
 }
 
 export default ImagenHotel;
