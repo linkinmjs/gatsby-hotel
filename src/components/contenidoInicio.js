b1037214d3c07ae59a3e0a669578435e88dc15c3
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-27 08:31:06 -0300

    CSS a la sección Principal

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const TextoInicio = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        column-gap: 2rem;
+    }
+
+    p{
+        line-height: 2;
+    }
+`
 
 const ContenidoInicio = () => {
 
@@ -24,12 +43,18 @@ const ContenidoInicio = () => {
 
     return (
         <>
-            <h2>{titulo}</h2>
+            <h2
+                css={css`
+                    text-align: center;
+                    font-size: 4rem;
+                    margin-top: 4rem;
+                `}
+            >{titulo}</h2>
 
-            <div>
+            <TextoInicio>
                 <p>{contenido}</p>
                 <GatsbyImage image={imagen.gatsbyImageData} alt="inicio" />
-            </div>
+            </TextoInicio>
 
         </>
     );
