ff41be7cfc71be8f223c6062a02eeedd04dffd3b
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-28 08:28:13 -0300

    CSS a Nosotros

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const Contenido = styled.main`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media ( min-width: 768px){
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 3rem;
+    }
+    p{
+        line-height: 2;
+        margin-top: 2rem;
+    }
+`;
 
 const ContenidoNosotros = () => {
 
@@ -20,19 +39,25 @@ const ContenidoNosotros = () => {
 
     const { titulo, contenido, imagen } = resultado.allDatoCmsPagina.nodes[0];
 
-        console.log(imagen);
+    console.log(imagen);
 
-    return ( 
+    return (
         <>
-            <h2>{titulo}</h2>
-            <div>
+            <h2
+                css={css`
+                    margin-top: 4rem;
+                    text-align: center;
+                    font-size: 4rem;
+                `}
+            >{titulo}</h2>
+            <Contenido>
                 <p>{contenido}</p>
-                <GatsbyImage 
-                    image={imagen.gatsbyImageData} alt="nosotros" 
+                <GatsbyImage
+                    image={imagen.gatsbyImageData} alt="nosotros"
                 />
-            </div>
+            </Contenido>
         </>
-     );
+    );
 }
- 
+
 export default ContenidoNosotros;
